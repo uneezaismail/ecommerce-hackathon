f2141d66baee2649954b4a68ba5808bd714ad15f
uneezaismail <ismailrahet@gmail.com>
2024-12-09 02:23:52 +0500

    update input

--- src/components/Billing.tsx
+++ src/components/Billing.tsx
@@ -3,25 +3,25 @@ import React from 'react'
 const Billing = () => {
   return (
  
-    <div className='w-[400px] mx-auto lg:w-[508px] h-fit xl:w-[608px] py-9 lg:pb-14 lg:px-16 space-y-9'>
+    <div className='w-full mx-4 sm:w-[300px] sm:mx-auto lg:w-[508px] h-fit xl:w-[608px] py-9 lg:pb-14 lg:px-16 space-y-9'>
   <div>
     <h2 className='text-4xl font-semibold'>Billing Details</h2>
   </div>
-  <form className="space-y-9 md:w-[470px]">
+  <form className="space-y-6 md:space-y-9 md:w-[470px]">
     {/* First Name and Last Name */}
     <div className="flex items-center gap-x-7">
       <div className="space-y-5">
         <label className="block text-black">First Name</label>
         <input
           type="text"
-          className="w-full border-2 border-gray-400 rounded-[10px] h-[60px] md:h-[75px] focus:outline-none focus:ring-0"
+          className="w-full border-2 border-gray-400 rounded-sm md:rounded-[10px] h-[40px] md:h-[75px] focus:outline-none focus:ring-0"
         />
       </div>
       <div className="space-y-5">
         <label className="block text-black">Last Name</label>
         <input
           type="text"
-          className="w-full border-2 border-gray-400 rounded-[10px] h-[60px] md:h-[75px] focus:outline-none focus:ring-0"
+          className="w-full border-2 border-gray-400 rounded-sm md:rounded-[10px] h-[40px] md:h-[75px] focus:outline-none focus:ring-0"
         />
       </div>
     </div>
@@ -31,7 +31,7 @@ const Billing = () => {
       <label className="block text-black">Company Name (Optional)</label>
       <input
         type="text"
-        className="w-full border-2 border-gray-400 rounded-[10px] h-[60px] md:h-[75px] focus:outline-none focus:ring-0"
+        className="w-full border-2 border-gray-400 rounded-sm md:rounded-[10px] h-[40px] md:h-[75px] focus:outline-none focus:ring-0"
       />
     </div>
 
@@ -39,7 +39,7 @@ const Billing = () => {
     <div className="space-y-5">
       <label className="block text-black">Country / Region</label>
       <select
-        className="w-full border-2 border-gray-400 rounded-[10px] h-[60px] md:h-[75px] focus:outline-none focus:ring-0 bg-white"
+        className="w-full border-2 border-gray-400 rounded-sm md:rounded-[10px] h-[40px] md:h-[75px] focus:outline-none focus:ring-0 bg-white"
       >
         <option value="">
           Select Country
@@ -55,7 +55,7 @@ const Billing = () => {
       <label className="block text-black">Street Address</label>
       <input
         type="text"
-        className="w-full border-2 border-gray-400 rounded-[10px] h-[60px] md:h-[75px] focus:outline-none focus:ring-0"
+        className="w-full border-2 border-gray-400 rounded-sm md:rounded-[10px] h-[40px] md:h-[75px] focus:outline-none focus:ring-0"
         placeholder="House number and street name"
       />
     </div>
@@ -65,7 +65,7 @@ const Billing = () => {
       <label className="block text-black">Town / City</label>
       <input
         type="text"
-        className="w-full border-2 border-gray-400 rounded-[10px] h-[60px] md:h-[75px] focus:outline-none focus:ring-0"
+        className="w-full border-2 border-gray-400 rounded-sm md:rounded-[10px] h-[40px] md:h-[75px] focus:outline-none focus:ring-0"
       />
     </div>
 
@@ -73,7 +73,7 @@ const Billing = () => {
     <div className="space-y-5">
       <label className="block text-black">Province</label>
       <select
-        className="w-full border-2 border-gray-400 rounded-[10px] h-[60px] md:h-[75px] focus:outline-none focus:ring-0 bg-white"
+        className="w-full border-2 border-gray-400 rounded-sm md:rounded-[10px] h-[40px] md:h-[75px] focus:outline-none focus:ring-0 bg-white"
       >
         <option value="">
           Select Province
@@ -88,7 +88,7 @@ const Billing = () => {
       <label className="block text-black">ZIP Code</label>
       <input
         type="text"
-        className="w-full border-2 border-gray-400 rounded-[10px] h-[60px] md:h-[75px] focus:outline-none focus:ring-0"
+        className="w-full border-2 border-gray-400 rounded-sm md:rounded-[10px] h-[40px] md:h-[75px] focus:outline-none focus:ring-0"
       />
     </div>
 
@@ -97,7 +97,7 @@ const Billing = () => {
       <label className="block text-black">Phone</label>
       <input
         type="text"
-        className="w-full border-2 border-gray-400 rounded-[10px] h-[60px] md:h-[75px] focus:outline-none focus:ring-0"
+        className="w-full border-2 border-gray-400 rounded-sm md:rounded-[10px] h-[40px] md:h-[75px] focus:outline-none focus:ring-0"
       />
     </div>
 
@@ -106,14 +106,14 @@ const Billing = () => {
       <label className="block text-black">Email Address</label>
       <input
         type="email"
-        className="w-full border-2 border-gray-400 rounded-[10px] h-[60px] md:h-[75px] focus:outline-none focus:ring-0"
+        className="w-full border-2 border-gray-400 rounded-sm md:rounded-[10px] h-[40px] md:h-[75px] focus:outline-none focus:ring-0"
       />
     </div>
 
     {/* Additional Information */}
     <input
       type="text"
-      className="w-full border-2 border-gray-400 rounded-[10px] h-[60px] md:h-[75px] focus:outline-none focus:ring-0"
+      className="w-full border-2 border-gray-400 rounded-sm md:rounded-[10px] h-[40px] md:h-[75px] focus:outline-none focus:ring-0"
       placeholder="Additional information"
     />
   </form>
