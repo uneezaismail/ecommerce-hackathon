4997209e39458ed42e07d5bf8f4d2a7122623d9e
uneezaismail <ismailrahet@gmail.com>
2025-01-20 04:37:32 +0500

    added suspense

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -1,7 +1,7 @@
 
 import NavImage from '@/components/NavImage';
 import Delivery from '@/components/ourDelivery';
-import React from 'react';
+import React, { Suspense } from 'react';
 import ShopPage from '@/components/FilterSortBar';
 
 const Shop = async () => {
@@ -9,7 +9,9 @@ const Shop = async () => {
   return (
     <section className="space-y-10">
       <NavImage heading={"Shop"} path={"Home"} currentPage={"Shop"} />
+      <Suspense fallback={<div>Loading...</div>}>
       <ShopPage/>
+      </Suspense>
       <Delivery />
     </section>
   );
